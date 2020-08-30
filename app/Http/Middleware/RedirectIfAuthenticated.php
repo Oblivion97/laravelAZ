20b88ff782b5f5aaef4d4d5fa320a8670c9a2858
hridoy97 <mahmudul.uiu041@gmail.com>
2020-08-31 02:28:42 +0600

    multi authentication logic middleware

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,26 +2,28 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 
 class RedirectIfAuthenticated
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
+     * @param Request $request
+     * @param Closure $next
+     * @param string|null $guard
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
-        }
+        if (Auth::guard($guard)->check() && Auth::user()->role->id == '1') {
+            //die("logged in");
 
-        return $next($request);
+            return redirect()->route('admin.dashboard');
+        }elseif(Auth::guard($guard)->check() && Auth::user()->role->id == '2') {
+            return redirect()->route('author.dashboard');
+        }else{ return $next($request); }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['as' => 'admin.' , 'prefix'=>'admin', 'namespace' => 'Admin' , 'middleware' =>['auth' , 'admin' ]],
+    function(){
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+    });
+
+Route::group(['as' => 'author.' , 'prefix'=>'author', 'namespace' => 'Author' , 'middleware' =>['auth' , 'author' ]],
+    function(){
+        Route::get('dashboard','DashboardController@index')->name('dashboard');
+    });
